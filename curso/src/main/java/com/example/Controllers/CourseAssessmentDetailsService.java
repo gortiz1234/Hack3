97b85cb67df104f7faba01a3cc3d31286679e23f
gortiz1234 <gustavo.ortiz@utec.edu.pe>
2023-10-29 14:01:35 -0500

    Commit de las clases que faltaban y de los controladores

---
+++ curso/src/main/java/com/example/Controllers/CourseAssessmentDetailsService.java
@@ -0,0 +1,45 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseAssessmentDetailsService {
+    private final CourseAssessmentDetailsRepository courseAssessmentDetailsRepository;
+
+    @Autowired
+    public CourseAssessmentDetailsService(CourseAssessmentDetailsRepository courseAssessmentDetailsRepository) {
+        this.courseAssessmentDetailsRepository = courseAssessmentDetailsRepository;
+    }
+
+    public List<CourseAssessmentDetails> getAllCourseAssessmentDetails() {
+        return courseAssessmentDetailsRepository.findAll();
+    }
+
+    public CourseAssessmentDetails getCourseAssessmentDetailsById(Long id) {
+        Optional<CourseAssessmentDetails> optionalDetails = courseAssessmentDetailsRepository.findById(id);
+        return optionalDetails.orElse(null);
+    }
+
+    public void createCourseAssessmentDetails(CourseAssessmentDetails details) {
+        courseAssessmentDetailsRepository.save(details);
+    }
+
+    public boolean updateCourseAssessmentDetails(Long id, CourseAssessmentDetails details) {
+        if (courseAssessmentDetailsRepository.existsById(id)) {
+            details.setId(id);
+            courseAssessmentDetailsRepository.save(details);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteCourseAssessmentDetails(Long id) {
+        if (courseAssessmentDetailsRepository.existsById(id)) {
+            courseAssessmentDetailsRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
--- curso/src/main/java/com/example/curso/entidades/CourseAssessment.java
+++ curso/src/main/java/com/example/curso/entidades/CourseAssessment.java
@@ -4,8 +4,85 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
+@Table(name = "courseassessment")
 public class CourseAssessment {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    public CourseAssessment(){}
+
+    @ManyToOne
+    @JoinColumn(name = "course")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "periodo")
+    private Periodo periodo;
+
+    private String title;
+    private String tipoNota;
+    private int numNota;
+    private String nomenclatura;
+
+    // Getter y setter para id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter y setter para course
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    // Getter y setter para periodo
+    public Periodo getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(Periodo periodo) {
+        this.periodo = periodo;
+    }
+
+    // Getter y setter para title
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Getter y setter para tipoNota
+    public String getTipoNota() {
+        return tipoNota;
+    }
+
+    public void setTipoNota(String tipoNota) {
+        this.tipoNota = tipoNota;
+    }
+
+    // Getter y setter para numNota
+    public int getNumNota() {
+        return numNota;
+    }
+
+    public void setNumNota(int numNota) {
+        this.numNota = numNota;
+    }
+
+    // Getter y setter para nomenclatura
+    public String getNomenclatura() {
+        return nomenclatura;
+    }
+
+    public void setNomenclatura(String nomenclatura) {
+        this.nomenclatura = nomenclatura;
+    }
 }
